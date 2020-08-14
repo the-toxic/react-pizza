fb9931adc64af2df75c98e76ee11afbdf61f434a
ToXiC <admin@toxic.pro>
2020-08-14 12:25:41 +0400

    Reactive pizza cards

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header'
 import Categories from "./Categories"
 import SortPopup from "./SortPopup"
+import Pizza from "./Pizza"
 
-export {Header, Categories, SortPopup}
+export {Header, Categories, SortPopup, Pizza}
